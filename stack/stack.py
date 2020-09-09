cac5ff93fded4d342d56b37ba82ab4b02a1f983b
Eric Taylor <erictaylor@Erics-MacBook-Pro.local>
2020-09-09 01:00:25 -0400

    stack - complete (except pop issue which comes from remove_tail method in singly_linked_list file

--- stack/stack.py
+++ stack/stack.py
@@ -10,16 +10,51 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+
+"""
+#implement the Stack using an array as the storage structure
+class Stack():
+    def __init__(self):
+        self.items = []
+    
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+    
+    def get_stack(self):
+        return self.items
+
+#test the stack functionality with an array
+s = Stack()
+s.push("A")
+s.push("B")
+print(s.get_stack())
+"""
+
+
+#####implement the Stack using linked list as the storage structure
+import sys
+sys.path.append("/Users/erictaylor/Documents/Lambda/Data-Structures/singly_linked_list/")
+from singly_linked_list import LinkedList
+
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList()
 
     def __len__(self):
-        pass
+        return self.size
 
     def push(self, value):
-        pass
+        self.storage.add_to_tail(value)
+        self.size +=1
 
     def pop(self):
-        pass
+        if self.size > 0:
+            self.size -= 1
+            return self.storage.remove_tail()
+        return None
+
+        
